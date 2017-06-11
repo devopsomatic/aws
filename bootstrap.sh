7ad3cdc709141105709964e3aa1a31f87cb1032f
nicerobot <nicerobot@users.noreply.github.com>
2017-06-11 13:01:22 -0700

    Create bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+NOW=$(date +%Y%m%dt%H%M%S)
+UNQ=${NOW}.${RANDOM}
+LOGD=/var/log/userdata
+LOG=${LOGD}/${SELFU}.${UNQ}.log
+
+exec 3>&1 4>&2
+exec 1>${LOG} 2>&1
+
+date
+echo args: ${@}
 set
-echo $(date) ${@}
+set -o verbose
+set -o errtrace
+set -o xtrace
+set -o nounset
+
+VERSION=${VERSION:-master}
+
+for installer in ${@:-salt debian/dev debian/java debian/docker debian/stats}; do
+  curl -sL -o- https://raw.githubusercontent.com/devopsomatic/aws/${VERSION}/userdata/${installer}.sh #| bash -vx -s --
+done
