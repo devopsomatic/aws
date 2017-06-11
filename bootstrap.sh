acfe2c469116d163b232553e07691b84921ae8fa
nicerobot <nicerobot@users.noreply.github.com>
2017-06-11 13:06:11 -0700

    Create bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ NOW=$(date +%Y%m%dt%H%M%S)
 UNQ=${NOW}.${RANDOM}
 LOGD=/var/log/userdata
 [ -w ${LOGD} ] || LOGD=${PWD}
-LOG=${LOGD}/${SELFU}.${UNQ}.log
+LOG=${LOGD}/bootstrap.${UNQ}.log
 
 exec 3>&1 4>&2
 exec 1>${LOG} 2>&1
